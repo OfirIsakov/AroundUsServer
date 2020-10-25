e1de8c2e8c6cc3981a01b30402a665913a8521f5
ofirisakov <ofirfifi@gmail.com>
2020-10-25 17:57:40 +0200

    Update player struct

--- main.go
+++ main.go
@@ -9,13 +9,22 @@ import (
 const port = 27403
 const host = "127.0.0.1"
 
+type position struct {
+	x float32
+	y float32
+	z float32
+}
+
 type player struct {
-	name        string
-	color       string
-	isImposter  bool
-	inVent      bool
-	isDead      bool
-	gotReported bool
+	name           string   // The name of the player, can contain anything
+	color          int8     // The index of the color in the color list held in the client
+	isImposter     bool     // Sent on the round start to tell the client if hes an imposter or crew
+	inVent         bool     // If true the server shouldnt send the player locations until hes leaving the vent
+	isDead         bool     // If the player is dead the server shouldnt send his location
+	gotReported    bool     // If the player didnt get reported yet tell the client to show a body on the ground
+	playerPosition position // The position of the player in Unity world cordinates
+	pitch          int      // Should be -90 <= pitch <= 90, represents the head pitch(Up and down)
+	rotation       int      // Should be 0 <= rotation <= 360, represents the body rotation
 }
 
 func main() {
